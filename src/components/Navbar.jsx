8837cf3b7fa6fbbe7037adeccc8617a2474723b4
Habd0231 <abdou0231henni@gmail.com>
2023-11-26 15:32:12 +0100

    added messages and notifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,14 @@ import {
   Typography,
   Menu,
   MenuItem,
+  Button,
+  Popover,
+  Card,
+  CardContent,
+  ListItem,
+  ListItemAvatar,
+  Divider,
+  ListItemText,
 } from "@mui/material";
 import React, { useState } from "react";
 import LogoDevIcon from "@mui/icons-material/LogoDev";
@@ -20,7 +28,7 @@ const StyledToolbar = styled(Toolbar)({
 });
 const Search = styled("div")(({ theme }) => ({
   backgroundColor: "background.default",
-  color:"text.primary",
+  color: "text.primary",
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
   width: "40%",
@@ -43,8 +51,27 @@ const UserBox = styled(Box)(({ theme }) => ({
     display: "none",
   },
 }));
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorElN, setAnchorElN] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClickNotification = (event) => {
+    setAnchorElN(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleCloseNotification = () => {
+    setAnchorElN(null);
+  };
+  const messageOpen = Boolean(anchorEl);
+  const notificationOpen = Boolean(anchorElN);
+
   return (
     <AppBar position="sticky" elevation={0} color={"default"}>
       <StyledToolbar>
@@ -56,14 +83,150 @@ const Navbar = () => {
           <InputBase placeholder="Search...." />
         </Search>
         <Icons>
-          <Badge badgeContent={4} color="error">
-            <Mail />
-          </Badge>
+            <Badge badgeContent={4} color="error">
+              <Mail onClick={handleClick} sx={{ cursor: 'pointer' }}/>
+            </Badge>
+          <Popover
+            open={messageOpen}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            <Card>
+              <CardContent>
+                <Typography variant="h5">Messages</Typography>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="https://material-ui.com/static/images/avatar/1.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Brunch this weekend?"
+                    secondary={
+                      <React.Fragment>
+                        {" — I'll be in your neighborhood doing errands this…"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="" />
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Travis Howard"
+                      src="https://material-ui.com/static/images/avatar/2.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Summer BBQ"
+                    secondary={
+                      <React.Fragment>
+                        {" — Wish I could come, but I'm out of town this…"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="" />
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Cindy Baker"
+                      src="https://material-ui.com/static/images/avatar/3.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Oui Oui"
+                    secondary={
+                      <React.Fragment>
+                        {" — Do you have Paris recommendations? Have you ever…"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Box textAlign="center">
+                  <Button variant="text">See all Messages</Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Popover>
           <Badge badgeContent={2} color="error">
-            <Notifications />
+            <Notifications onClick={handleClickNotification} sx={{ cursor: 'pointer' }}/>
           </Badge>
+          <Popover
+            open={notificationOpen}
+            anchorEl={anchorElN}
+            onClose={handleCloseNotification}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            <Card>
+              <CardContent>
+                <Typography variant="h5">Notifications</Typography>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="https://material-ui.com/static/images/avatar/1.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Brunch this weekend?"
+                    secondary={
+                      <React.Fragment>
+                        {" — replied to your comment"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="" />
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Travis Howard"
+                      src="https://material-ui.com/static/images/avatar/2.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Summer BBQ"
+                    secondary={
+                      <React.Fragment>
+                        {" — commented on your post"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="" />
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Cindy Baker"
+                      src="https://material-ui.com/static/images/avatar/3.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Oui Oui"
+                    secondary={
+                      <React.Fragment>
+                        {" — Liked your comment"}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Box textAlign="center">
+                  <Button variant="text">See all Notifications</Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Popover>
           <Avatar
-            sx={{ width: 30, height: 30 }}
+            sx={{ width: 30, height: 30, cursor: 'pointer' }}
             src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             onClick={(e) => setOpen(true)}
           />
@@ -80,7 +243,7 @@ const Navbar = () => {
         id="demo-positioned-menu"
         aria-labelledby="demo-positioned-button"
         open={open}
-        onClose={e=>setOpen(false)}
+        onClose={(e) => setOpen(false)}
         anchorOrigin={{
           vertical: "top",
           horizontal: "right",
